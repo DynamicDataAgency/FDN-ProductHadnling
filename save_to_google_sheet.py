0a1e4d262435e1a3663457a8eaec23b54eeda69a
victoriashopify <eduardo@dynamicdata.com>
2024-09-27 22:06:33 +0200

    Update save_to_google_sheet.py

--- save_to_google_sheet.py
+++ save_to_google_sheet.py
@@ -11,23 +11,7 @@ dotenv.load_dotenv()
 # Define the scope
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/drive']
 
-# Save the credentials to a JSON file
-CredentialsJson = {
-    "type": os.getenv("TYPE"),
-    "project_id": os.getenv("PROJECT_ID"),
-    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-    "private_key": os.getenv("PRIVATE_KEY"),
-    "client_email": os.getenv("CLIENT_EMAIL"),
-    "client_id": os.getenv("CLIENT_ID"),
-    "auth_uri": os.getenv("AUTH_URI"),
-    "token_uri": os.getenv("TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
-}
 
-# Save to a JSON file
-with open('credentials.json', 'w') as json_file:
-    json.dump(CredentialsJson, json_file)
 
 # Authorize using the JSON file
 creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
