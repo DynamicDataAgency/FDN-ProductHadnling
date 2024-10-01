c4def0b1814aefdb24ff47a8a5e46b6f151b553b
victoriashopify <eduardo@dynamicdata.com>
2024-10-01 21:21:29 +0200

    Update save_to_google_sheet.py

--- save_to_google_sheet.py
+++ save_to_google_sheet.py
@@ -53,7 +53,7 @@ credentials = service_account.Credentials.from_service_account_info({
 
 
 
-creds = ServiceAccountCredentials.from_json_keyfile_name("crds.json") 
+creds = ServiceAccountCredentials.from_json_keyfile_name("crds.json", scopes=scopes) 
 
 client = gspread.authorize(creds)
 
